13c1002f19726be0497398e47030a8c8e9c7fb67
Zahid <zainab.12.zahid@gmail.com>
2026-05-06 00:12:12 +0100

    Ajout CI/CD GitHub Actions + corrections imports

--- client-service/src/test/java/com/example/clientservice/service/ClientIntegrationTest.java
+++ client-service/src/test/java/com/example/clientservice/service/ClientIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;

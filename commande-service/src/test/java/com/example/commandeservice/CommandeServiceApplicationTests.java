5d90c51ded87077bafb0b75af4cf06c3b80911ad
jihane <j.nasri5338@uca.ac.ma>
2026-05-10 18:22:08 +0100

    Correction commande-service

--- commande-service/src/test/java/com/example/commandeservice/CommandeServiceApplicationTests.java
+++ commande-service/src/test/java/com/example/commandeservice/CommandeServiceApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.commandeservice;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CommandeServiceApplicationTests {
     @Test
